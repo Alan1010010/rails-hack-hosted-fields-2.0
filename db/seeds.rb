c0e9cd7a2d63b2ca4e81c059f0094ec24bfb98f2
Alan Quinn <alancquinn2@gmail.com>
2020-10-08 16:29:55 +0100

    some things and some stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Plan.destroy_all
+
+puts "throwing some seed"
 
   Plan.create(
     name: "Pro",
-    price: 99
+    price: 99,
+    plan_id: 1592
   )
 
     Plan.create(
     name: 'Premium',
-    price: 299
+    price: 299,
+    plan_id: 1591
   )
 
       Plan.create(
     name: 'Enterprise',
-    price: 499
+    price: 499,
+    plan_id: 1593
   )
+
+puts "all done y'alllllll"
 
